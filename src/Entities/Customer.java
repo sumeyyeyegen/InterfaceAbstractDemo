7f6a1fb632fad4a3a2c67d24df3942ea9197326b
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-03 23:00:23 +0300

    classes added.

---
+++ src/Entities/Customer.java
@@ -0,0 +1,42 @@
+package Entities;
+
+import Abstract.Entity;
+
+public class Customer implements Entity {
+	private int id;
+	private String FirstName;
+	private String lastName;
+	private String birthYear;
+	private String NationalityId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(String birthYear) {
+		this.birthYear = birthYear;
+	}
+	public String getNationalityId() {
+		return NationalityId;
+	}
+	public void setNationalityId(String nationalityId) {
+		NationalityId = nationalityId;
+	}
+}
